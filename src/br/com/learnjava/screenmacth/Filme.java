c71431af4d37084c01532cf7c81fe39c7d694242
llucasBorba <lucasborba6075@gmail.com>
2023-07-04 12:32:12 -0300

    remover comentario

---
+++ src/br/com/learnjava/screenmacth/Filme.java
@@ -0,0 +1,14 @@
+package br.com.learnjava.screenmacth;
+
+
+public class Filme extends Titulo {
+ private String diretor;
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+}
