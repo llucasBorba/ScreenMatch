37996a07d33e167a31389feac39ebcda26df4b2d
llucasBorba <lucasborba6075@gmail.com>
2023-08-23 19:22:08 -0300

    Mudança em exception

--- src/br/com/learnjava/screenmacth/execptions/ErroNoAnoException.java
+++ src/br/com/learnjava/screenmacth/execptions/ErroNoAnoException.java
@@ -5,6 +5,6 @@ public class ErroNoAnoException extends RuntimeException {
 
     @Override
     public String getMessage() {
-        return "Ouve um erro no tamanho do ano !";
+        return "Ouve um erro no tamanho do ano!";
     }
 }
